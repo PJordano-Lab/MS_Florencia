5a095214e648dc0f80da591a0af18a4249d0673b
Pedro Jordano <jordano@ebd.csic.es>
2017-07-20 14:22:45 +0200

    Final resamp code for test.

--- code/resamp.R
+++ code/resamp.R
@@ -1,5 +1,9 @@
+# Fully randomized assignment of the GRAZED dataset, no. resamps: 999.
+# I trimm the grazed dataset to delete trees so that I get N=61.
+# Trees with 0 records are excluded.
+# Datasets from the G and UG sites have now 61x12
 #
-rndmz<-10                     # Number of randomizations required
+rndmz<- 999                     # Number of randomizations required
 rand_qobs<- NULL                # QAP q value for observed matrix in each resampling.
 rand_q_mean_resamp<- NULL       # mean QAP q value for 1000 random marices of each resampling.
 rand_hdist_obs<- NULL           # Hamming h value for observed matrix in each resampling.
@@ -24,5 +28,5 @@ for (i in 1:rndmz) {
   rand_hdist_obs<- append(rand_hdist_obs, q.prosop$testval)
   rand_hdist_mean_resamp<- append(rand_hdist_mean_resamp, mean(q.prosop$dist))
 }
-
-#-----------------------------
+plot(density(rand_hdist_mean_resamp), xlim= c(min(rand_hdist_obs), max(rand_hdist_mean_resamp)))
+abline(v= mean(rand_hdist_obs), col = "blue", lty = 3)
